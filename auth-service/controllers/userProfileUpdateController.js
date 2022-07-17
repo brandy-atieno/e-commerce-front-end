4f4ac973c3d65bb657eaeac1ac9a35568352f4c8
Vinton Juniour <vintonmogakajuniour@gmail.com>
2022-07-17 14:59:57 +0300

    resolved authenticationController.js  conflict

--- auth-service/controllers/userProfileUpdateController.js
+++ auth-service/controllers/userProfileUpdateController.js
@@ -1,24 +1,30 @@
 const { user } = require('../config/config');
 const mssql = require('mssql')
+const bcrypt = require('bcryptjs')
 const poolPromise = require('../config/poolPromise')
 
 module.exports = {
-    userProfileUpdate: async(req, res) => {
-        let { user_name, email, password } = req.body
-        let pool = await poolPromise()
-        pool.request()
-            .input('user_name', user_name)
-            .input('email', email)
-            .input('password', password)
-            .input('StatementType', 'Update')
-            .execute('dbo.register_queries')
+    updateUserProfile: async(req, res) => {
 
-        .then(results => {
-            if (results.rowsAffected) {
-                res.send("Updated!")
-                console.log("Updated!")
+        if (req.body.userId === req.params.id) {
+            if (req.body.password) {
+                try {
+                    const salt = await bcrypt.genSalt(10);
+                    req.body.password = await bcrypt.hash(req.body.password, salt);
+                } catch (err) {
+                    return res.status(500).json(err);
+                }
             }
-        })
-
+            try {
+                const user = await User.findByIdAndUpdate(req.params.id, {
+                    $set: req.body,
+                });
+                res.status(200).json("Account has been updated");
+            } catch (err) {
+                return res.status(500).json(err);
+            }
+        } else {
+            return res.status(403).json("You can update only your account!");
+        }
     }
 }
